e884c6cb967aa43aeb81aec38518911ab973dc72
Sam <gentalmen28@gmail.com>
2026-03-03 16:25:11 +0000

    Extracting the Emails from Inbox

--- app/api/google/route.ts
+++ app/api/google/route.ts
@@ -12,9 +12,34 @@ export async function GET() {
   const userId = session.user.id;
   const gmailClient = await getGmailClient(userId);
 
-  const profile = await gmailClient.users.getProfile({
+  const listResposne  = await gmailClient.users.messages.list({
     userId: "me",
+    maxResults: 5,
+    q: "in:inbox"
   });
+  const messages = listResposne.data.messages || [];
+  const emails = []
 
-  return Response.json(profile.data);
+  for(const message of messages){
+    const fullMessage = await gmailClient.users.messages.get({
+        userId: "me",
+        id: message.id!,
+        format: "metadata"
+    });
+    
+    const headers = fullMessage.data.payload?.headers || []
+    const subject = headers.find(h => h.name === "Subject")?.value ||  "";
+    const from = headers.find(h=> h.name === "From")?.value || "";
+
+    emails.push({
+        id: fullMessage.data.id,
+        snippet: fullMessage.data.snippet,
+        subject,
+        from,
+        internalDate: fullMessage.data.internalDate,
+    })
+  }
+
+
+  return Response.json(emails);
 }
