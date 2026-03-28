74b93537223a90316ebb1d49b8713dc0ed5fec2e
Sam <gentalmen28@gmail.com>
2026-03-28 21:36:46 +0000

    Added button for easy access

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <div>
-    
-   </div>
+    <div className="flex items-center justify-center h-40 ">
+      <Button className="p-10 bg-red-500 text-amber-100 text-2xl">
+        <Link href="api/auth/signin"> Google Sign In</Link>
+      </Button>
+          <Button className="p-10 bg-red-500 text-amber-100 text-2xl">
+        <Link href="/api/google"> Check Gmail</Link>
+      </Button>
+    </div>
   );
 }
